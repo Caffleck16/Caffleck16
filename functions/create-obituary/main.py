9f834480ca2fb328a0243a8ad3319526420da64d
carson-may <carson.may@ucalgary.ca>
2023-04-09 13:41:47 -0600

    some work on create_obituary

--- functions/create-obituary/main.py
+++ functions/create-obituary/main.py
@@ -1,16 +1,20 @@
 import boto3
 import json
 import requests
-import requests_toolbelt.multipart as multipart
+from requests_toolbelt.multipart import decoder
 import base64
 from boto3.session import Session
-from botocore.exceptions import BotoCoreError, ClientError
+import os
+import hmac
+import time
+import hashlib
 
 ssm = boto3.client('ssm')
 response = ssm.get_parameters(
     Names=[
         'chatgpt_api_key',
         'cloudinary_api_key',
+        'cloudinary_secret_key',
         'accessKey',
         'secretAccessKey'
     ],
@@ -21,62 +25,43 @@ params = response['Parameters']
 
 chatgpt_api_key = next(p['Value'] for p in params if p['Name'] == 'chatgpt_api_key')
 cloudinary_api_key = next(p['Value'] for p in params if p['Name'] == 'cloudinary_api_key')
+cloudinary_secret_key = next(p['Value'] for p in params if p['Name'] == 'cloudinary_secret_key')
 aws_access_key_id = next(p['Value'] for p in params if p['Name'] == 'accessKey')
 aws_secret_access_key = next(p['Value'] for p in params if p['Name'] == 'secretAccessKey')
 region_name = 'ca-central-1'
 
 polly_client = boto3.client('polly')
 
+def generate_cloudinary_signature(params):
+    sorted_params = sorted(params.items())
+    to_sign = '&'.join(['='.join(kv) for kv in sorted_params])
+    to_sign += cloudinary_secret_key
+    signature = hmac.new(cloudinary_secret_key.encode('utf-8'), to_sign.encode('utf-8'), hashlib.sha256).hexdigest()
+    return signature
+
 def lambda_handler(event, context):
     try:
         # read the data from the request body
-        data = multipart.decoder.MultipartDecoder(event['body'], event['headers']['Content-Type']).parts[0]
-        decoded_data = base64.b64decode(data.content).decode('utf-8')
-        print(decoded_data)
-        obituary_data = json.loads(decoded_data)
+        body = event['body']
+        if event['isBase64Encoded']:
+            body = base64.b64decode(body)
+        content_type = event["headers"]["Content-Type"]
+        data = decoder.MultipartDecoder(body, content_type)
+        obituary_data = [part.content for part in data.parts]
         print(obituary_data)
 
         # parse the obituary data
-        name = obituary_data['name']
-        born_year = obituary_data['born_year']
-        died_year = obituary_data['died_year']
-        image_url = obituary_data['image_url']
+        name = obituary_data[1].decode()
+        born_year = obituary_data[2].decode()
+        died_year = obituary_data[3].decode()
+        key = name + ".png"
+        file_name = os.path.join("/tmp", key)
+        with open(key, "wb") as f:
+                f.write(obituary_data[0])
 
         # generate the obituary text using ChatGPT
         chatgpt_url = 'https://api.openai.com/v1/completions'
         chatgpt_prompt = f'write an obituary about a fictional character named {name} who was born on {born_year} and died on {died_year}.'
-        chatgpt_data = {
-            "prompt": chatgpt_prompt,
-            "max_tokens": 600,
-            "model": "text-curie-001"
-        }
-        chatgpt_headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {chatgpt_api_key}"
-        }
-        chatgpt_response = requests.post(chatgpt_url, headers=chatgpt_headers, json=chatgpt_data)
-        chatgpt_obituary = chatgpt_response.json()['choices'][0]['text']
-        
-        # convert the obituary text to speech using Amazon Polly
-        response = polly_client.synthesize_speech(VoiceId='Joanna', OutputFormat='mp3', Text=chatgpt_obituary)
-        audio_stream = response['AudioStream'].read()
-
-        # upload the audio to Cloudinary
-        cloudinary_url = 'https://api.cloudinary.com/v1_1/deiepuv9m/video/upload'
-        cloudinary_params = {
-            'api_key': cloudinary_api_key,
-            'upload_preset': 'obituaries_preset',
-            'e_art:zorro': '',
-            'resource_type': 'video'
-        }
-        cloudinary_headers = {
-            'Content-Type': 'application/octet-stream',
-            'X-Requested-With': 'XMLHttpRequest'
-        }
-        session = Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
-        s3 = session.client('s3')
-        # save audio file to S3
-        s3.upload_fileobj(response['AudioStream'], 'terraform-20230407223808852200000001')
         chatgpt_data = {
             "prompt": chatgpt_prompt,
             "max_tokens": 600,
@@ -100,16 +85,26 @@ def lambda_handler(event, context):
 
         # upload the speech mp3 file to Cloudinary
         cloudinary_url = f"https://api.cloudinary.com/v1_1/{cloudinary_api_key}/upload"
-        cloudinary_signature = ssm.get_parameter(Name='/cloudinary_signature', WithDecryption=True)['Parameter']['Value']
+        cloudinary_signature = generate_cloudinary_signature({"public_id" : name, "timestamp" : str(int(time.time()))})
         cloudinary_payload = {
             "file": (name + '.mp3', speech_content, 'audio/mp3'),
             "upload_preset": "obituaries",
             "signature": cloudinary_signature
         }
-        cloudinary_response = requests.post(cloudinary_url, files=cloudinary_payload)
+        cloudinary_audio_response = requests.post(cloudinary_url, files=cloudinary_payload)
+
+        # Upload image to cloudinary
+        with open(key, "rb") as f:
+            cloudinary_payload = {
+                "file": (file_name, f),
+                "upload_preset": "obituaries",
+                "signature": cloudinary_signature
+            }
+        cloudinary_image_response = requests.post(cloudinary_url, files=cloudinary_payload)
+        image_url = cloudinary_image_response.json()['secure_url']
 
         # get the public URL of the uploaded speech mp3 file
-        speech_url = cloudinary_response.json()['secure_url']
+        speech_url = cloudinary_audio_response.json()['secure_url']
 
         # add the e_art:zorro enhancement to the image URL
         image_url = image_url.replace('/upload/', '/upload/e_art:zorro/')
